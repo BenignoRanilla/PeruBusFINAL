1d22483974f09cb24b293790afa24c2a9a89fbae
Benigno Ranilla <joseb_ranilla@outlook.com>
2022-01-28 15:05:03 -0500

    update vistas 28/01

---
+++ app/src/main/java/com/ruta/perubus/RecoverypassActivity.kt
@@ -0,0 +1,11 @@
+package com.ruta.perubus
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RecoverypassActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_recoverypass)
+    }
+}
