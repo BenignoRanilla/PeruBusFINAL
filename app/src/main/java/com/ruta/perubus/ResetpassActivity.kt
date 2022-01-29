5321f4223ee053acee1f194f4f6489689be14392
Benigno Ranilla <joseb_ranilla@outlook.com>
2022-01-28 23:04:49 -0500

    update vistas 28/01 v2

---
+++ app/src/main/java/com/ruta/perubus/ResetpassActivity.kt
@@ -0,0 +1,11 @@
+package com.ruta.perubus
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ResetpassActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_resetpass)
+    }
+}
