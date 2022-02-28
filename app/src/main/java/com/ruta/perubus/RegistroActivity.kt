d9f246dd0fb9014cd1c5a0dda5390692bbaa97b1
Renato Guevara <renatoguevara16@gmail.com>
2022-02-28 09:43:17 -0500

    fix register activity

--- app/src/main/java/com/ruta/perubus/RegistroActivity.kt
+++ app/src/main/java/com/ruta/perubus/RegistroActivity.kt
@@ -1,93 +1,89 @@
 package com.ruta.perubus
 
 import android.content.Intent
-import android.net.DnsResolver
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.CheckBox
-import android.widget.EditText
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.ruta.perubus.api.Api
 import com.ruta.perubus.api.RetrofitClient
+import com.ruta.perubus.databinding.ActivityRegistroBinding
 import com.ruta.perubus.models.User
 import retrofit2.Call
 import retrofit2.Response
-import javax.security.auth.callback.Callback
 
 class RegistroActivity : AppCompatActivity() {
 
-    val login: Button = findViewById(R.id.LoginCreate)
-    val nroCelular: EditText = findViewById(R.id.nroCelular)
-    val correo: EditText = findViewById(R.id.correo)
-    val nombre: EditText = findViewById(R.id.nombre)
-    val apellido: EditText = findViewById(R.id.apellido)
-    val pass: EditText = findViewById(R.id.Pass)
-    val repeatPass: EditText = findViewById(R.id.passRepeat)
+    private lateinit var binding: ActivityRegistroBinding
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registro)
+        binding = ActivityRegistroBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val context = this
-
-//        val check: CheckBox = findViewById(R.id.checkBoxRegistro)
-        val intent = Intent(this, CredentialActivity::class.java)
-
-        login.setOnClickListener {
+        binding.LoginCreate.setOnClickListener {
             val newUser = User()
-            newUser.nroCelular = nroCelular.text.toString()
-            newUser.correo = correo.text.toString()
-            newUser.nombres = nombre.text.toString()
-            newUser.apellidos = apellido.text.toString()
-            newUser.contrasenia = pass.text.toString()
+            newUser.nroCelular = binding.nroCelular.text.toString()
+            newUser.correo = binding.correo.text.toString()
+            newUser.nombres = binding.nombre.text.toString()
+            newUser.apellidos = binding.apellido.text.toString()
+            newUser.contrasenia = binding.Pass.text.toString()
             newUser.origen = "app"
 
             val apiService = RetrofitClient.buildService(Api::class.java)
             val requestCall = apiService.createUser(newUser)
 
-            requestCall.enqueue(object: retrofit2.Callback<User>{
+            requestCall.enqueue(object : retrofit2.Callback<User> {
 
-                override fun onResponse(call: Call<User>, response: Response<User>){
-                    if (response.isSuccessful){
+                override fun onResponse(call: Call<User>, response: Response<User>) {
+                    if (response.isSuccessful) {
                         finish()
 //                        check.isChecked
                         var newCreateUser = response.body()
                         validarCampos()
-                        Toast.makeText(context, "Successfully Added", Toast.LENGTH_SHORT).show()
+                        Toast.makeText(applicationContext, "Successfully Added", Toast.LENGTH_SHORT)
+                            .show()
+                        val intent = Intent(applicationContext, CredentialActivity::class.java)
                         startActivity(intent)
-                    }else{
-                        Toast.makeText(context, "Failed to add item", Toast.LENGTH_SHORT).show()
+                    } else {
+                        Toast.makeText(applicationContext, "Failed to add item", Toast.LENGTH_SHORT)
+                            .show()
                     }
                 }
 
                 override fun onFailure(call: Call<User>, t: Throwable) {
-                    Toast.makeText(context, "Failed to add item", Toast.LENGTH_SHORT).show()
+                    Toast.makeText(applicationContext, "Failed to add item", Toast.LENGTH_SHORT)
+                        .show()
                 }
             })
 
-            }
+        }
     }
 
     private fun validarCampos(): Boolean {
         var datosCorrectos = true
         val context = this
 
-        if (nroCelular.text.toString().isNotEmpty()){
-            Toast.makeText(context, "Debe de ingresar un número de celular", Toast.LENGTH_SHORT).show()
+        if (binding.nroCelular.text.toString().isNotEmpty()) {
+            Toast.makeText(context, "Debe de ingresar un número de celular", Toast.LENGTH_SHORT)
+                .show()
             datosCorrectos = false;
-        }else if(correo.text.toString().isNotEmpty()){
+        } else if (binding.correo.text.toString().isNotEmpty()) {
             Toast.makeText(context, "Debe de ingresar un correo", Toast.LENGTH_SHORT).show()
             datosCorrectos = false;
-        }else if(nombre.text.toString().isNotEmpty()){
+        } else if (binding.nombre.text.toString().isNotEmpty()) {
             Toast.makeText(context, "Debe de ingresar un nombre", Toast.LENGTH_SHORT).show()
             datosCorrectos = false;
-        }else if(apellido.text.toString().isNotEmpty()) {
+        } else if (binding.apellido.text.toString().isNotEmpty()) {
             Toast.makeText(context, "Debe de ingresar un contraseña", Toast.LENGTH_SHORT).show()
             datosCorrectos = false;
-        }else if(!pass.text.toString().isNotEmpty() || !repeatPass.text.toString().isNotEmpty()) {
-            if(!pass.text.toString().equals(repeatPass.text.toString())){
-                Toast.makeText(context, "Las contraseñas deben de coincidir", Toast.LENGTH_SHORT).show()
+        } else if (!binding.Pass.text.toString().isNotEmpty() || !binding.passRepeat.text.toString()
+                .isNotEmpty()
+        ) {
+            if (!binding.Pass.text.toString().equals(binding.passRepeat.text.toString())) {
+                Toast.makeText(context, "Las contraseñas deben de coincidir", Toast.LENGTH_SHORT)
+                    .show()
                 datosCorrectos = false;
             }
         }
